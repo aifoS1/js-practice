661ecc0c1459bee5e2ad4b0ff1c2dff35e68fe16
Sofia Petsoulakis <petsofia@gmail.com>
2015-04-29 13:56:17 -0400

    Added contact form

--- phpapp/contact.php
+++ phpapp/contact.php
@@ -1,4 +1,19 @@
-<?php 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  $name = $_POST["name"];
+		$email = $_POST["email"];
+		$message = $_POST["message"];
+		$email_body = "";
+		$email_body = $email_body . "Name: " . $name . "\n";
+		$email_body = $email_body . "Email: " . $email . "\n";
+		$email_body = $email_body . "Message: " . $message;
+
+	//TODO: send email
+	  header("Location: contact.php?status=thanks");
+	  exit;
+}
+
+?><?php 
 $pageTitle = "Contact Mike";
 $section = "contact";
 include('inc/header.php'); ?>
@@ -7,39 +22,54 @@ include('inc/header.php'); ?>
 
 		<div class="wrapper">
 
-           <h1>Contact</h1>
+      <h1>Contact</h1>
+      <?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
+      <p>Thanks for the email! I&rsquo;ll be in touch shortly.</p>
+        <?php } else { ?>
 
-           <p>I&rsquo;d love to hear from you! Complete the form to send me an email</p>
+					      <p>I&rsquo;d love to hear from you! Complete the form to send me an email</p>
 
-           <form method="post">
-           	<table>
-           	  <tr>
-           		  <th>
-           		   	<label for="name">Name</label>
-           		  </th>
-           	  <td> 
-           	 	  <input type="text" name="name" id="name">
-           	  </td>
-              </tr>
-              <tr>
-           		  <th>
-           		   	<label for="email">Email</label>
-           		  </th>
-           	    <td> 
-           	 	    <input type="email" name="email" id="email">
-           	    </td>
-              </tr>
-              <tr>
-           		  <th>
-           		   	<label for="message">Message</label>
-           		  </th>
-           	    <td> 
-           	 	    <textarea type="text" name="message" id="message"></textarea>
-           	    </td>
-              </tr>
-             </table>
-             <input type="submit" value="SEND">
-           </form>
+					        <form method="post" action="contact.php">
+					        	<table>
+					        		<tr>
+					        			<th>
+					        				<label for="name">Name</label>
+					        			</th>
+					        			<td> 
+					        				<input type="text" name="name" id="name">
+					        			</td>
+					        		</tr>
+					        		<tr>
+					        			<th>
+					        				<label for="email">Email</label>
+					        			</th>
+					        			<td> 
+					        				<input type="email" name="email" id="email">
+					        			</td>
+					        		</tr>
+					        		<tr>
+					        			<th>
+					        				<label for="message">Message</label>
+					        			</th>
+					        			<td> 
+					        				<textarea type="text" name="message" id="message"></textarea>
+					        			</td>
+					        		</tr>
+					        		<tr>
+					        			<th>
+					        				<label for="reason">Reason for Inquiry</label>
+					        			</th>
+                        <td> 
+					        		    <select id="reason">
+					        			    <option value="buy a shirt" name="reason">Buy a Shirt</option>
+					        			    <option value="question about price" name="reason">Question about price</option>
+                        </td>
+					        		</select>
+					        		</tr>
+					        	</table>
+					        	<input type="submit" value="SEND">
+					        </form>
+           <?php }?>
 		</div>
 
 	</div>
